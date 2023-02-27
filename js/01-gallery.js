dbc21fb08733afefa5389c3086409303c63037ba
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-02-27 23:35:20 +0200

    Task-01 - add markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallaryContainerEl = document.querySelector('div.gallery');
+gallaryContainerEl.addEventListener('click', handlerGallaryContainerClick);
+
+const gallaryItemsMurkup = createGalleryItemsMurkup(galleryItems);
+gallaryContainerEl.insertAdjacentHTML('beforeend', gallaryItemsMurkup);
+
+function createGalleryItemsMurkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>
+            `;
+    }).join('');;    
+}
+
+function handlerGallaryContainerClick(e) {
+    e.preventDefault();
+    
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    } 
+   
+    console.log(e.target.dataset.source);
+}
+
+
+
