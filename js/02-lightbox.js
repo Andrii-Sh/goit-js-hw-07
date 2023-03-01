208a2ab3ccfd1f2fd31033d573d6f444b796a4f8
Andrii-Sh <114801650+Andrii-Sh@users.noreply.github.com>
2023-03-01 12:07:52 +0200

    task-02 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryContainerEl = document.querySelector('ul.gallery');
+
+const gallaryItemsMurkup = createGalleryItemsMurkup(galleryItems);
+gallaryContainerEl.insertAdjacentHTML('beforeend', gallaryItemsMurkup);
+
+function createGalleryItemsMurkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            `;
+    }).join('');;    
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    'captionsData': 'alt',
+    'captionDelay': 250
+});
+
+
